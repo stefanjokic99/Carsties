aa2371ed407fe5065ab96e7b74686fd99f01dc04
stefanjokic99 <105276465+stefansingularity@users.noreply.github.com>
2024-01-18 23:32:31 +0100

    End of section 2 - AuctionService

---
+++ src/AuctionService/RequestHelpers/MappingProfiles/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(dst => dst.Item, opt => opt.MapFrom(src => src));
+        CreateMap<CreateAuctionDto,Item>();
+    }
+}
